7a546d12fe3b90908e740dbebc36f6e255b972b1
harishmahadhev <harishmahadhev@gmail.com>
2021-08-07 14:53:29 +0530

    ..

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import {
-  BrowserRouter as Router,
+  BrowserRouter as Router, Route, Switch,
 } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <Switch>
+        <Route path="*"><App /></Route>
+      </Switch>
       <App />
     </Router>
+
   </React.StrictMode>,
   document.getElementById("root")
 );
